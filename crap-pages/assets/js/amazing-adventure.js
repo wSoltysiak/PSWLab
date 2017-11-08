939d7ab1a584ee9be592879a9db94787c11c2d86
Wojciech Sołtysiak <soltysiak.wojciech@gmail.com>
2017-11-08 17:26:03 +0100

    Poprawki do lintera

--- crap-pages/assets/js/amazing-adventure.js
+++ crap-pages/assets/js/amazing-adventure.js
@@ -1,25 +1,24 @@
-
-function niesamowitaFunkcja(){
+function niesamowitaFunkcja() {
     var tekst = window.prompt("Dostąpił cię zaszczyt ujrzenia tego niezwykłego okienka. Napisz jak bardzo się cieszysz!");
-    tekst += " ~ <small>Ktoś wspaniały</small>"
-    var places = document.getElementsByClassName("lovely-quotes")[0].innerHTML = tekst;
+    tekst += " ~ <small>Ktoś wspaniały</small>";
+    document.getElementsByClassName("lovely-quotes")[0].innerHTML = tekst;
     document.getElementsByClassName("lovely-quotes")[0].style.display = "block";
 }
 
-function przepięknyAlert(){
-    window.alert("Oh, jakże twe okno jest dostojne! Zostań i podziwiaj je jak najdłużej wędrowcze.")
+function przepięknyAlert() {
+    window.alert("Oh, jakże twe okno jest dostojne! Zostań i podziwiaj je jak najdłużej wędrowcze.");
 }
 
-function losujMagiczneZaklęcia(){
+function losujMagiczneZaklęcia() {
     var spells = ["Abra", "Kadabra", "Czary", "Mary", "Hokus", "Pokus", "Siabidi bim", "Siabidi bom", "Alohomora"];
 
-    totalTag = "";
+    var totalTag = "";
 
-    for (i = 0; i < 20; i++){
-        var tags = "<br>"
-        tags += "<span class='spell-bar'>"
-        for (j = 0; j < Math.random() * 20; j++){
-            tags += spells[parseInt(Math.random()*8)] + " ";
+    for (var i = 0; i < 20; i++) {
+        var tags = "<br>";
+        tags += "<span class='spell-bar'>";
+        for (var j = 0; j < Math.random() * 20; j++) {
+            tags += spells[Math.floor(Math.random() * spells.length)] + " ";
         }
         tags += "</span>";
         totalTag += tags;
@@ -28,12 +27,7 @@ function losujMagiczneZaklęcia(){
 }
 
 document.getElementsByClassName("lovely-quotes")[0].style.display = "none";
-
 window.addEventListener("load", przepięknyAlert);
-
 document.getElementById("magic-area").innerHTML = "<span class='magical-bar'>Zabiorę cię w świat magii - świat javascriptu</span>" + losujMagiczneZaklęcia();
-
-
 document.getElementById("guziczunio").addEventListener("click", niesamowitaFunkcja);
-
-document.writeln("<h2>Dziękuję że mogłem być dziś twym internetowym przewodnikiem.</h2>")
+document.write("<h2>Dziękuję że mogłem być dziś twym internetowym przewodnikiem.</h2>");
