ab06f06ad9d36cf6cea25b8bd447295725a90cb7
Wojciech Sołtysiak <soltysiak.wojciech@gmail.com>
2017-11-12 17:16:10 +0100

    Dodanie trybu disco

--- crap-pages/assets/js/game-mouse-events.js
+++ crap-pages/assets/js/game-mouse-events.js
@@ -1,13 +1,11 @@
 (function() {
     const gameField = document.querySelector('#game-field');
-
-    // gameField.onclick = event => {
-    //     console.log(event.clientX, event.clientY);
-    //     console.log(event.screenX, event.screenY);
-    // };
+    let oldX = undefined;
+    let oldY = undefined;
 
     gameField.onmouseover = addShadow;
     gameField.onmouseout = removeShadow;
+    window.onmousemove = calculateDiscoMode;
 
     function addShadow() {
         gameField.style.boxShadow = '10px 10px 10px #AAAAAA';
@@ -15,5 +13,20 @@
 
     function removeShadow() {
         gameField.style.boxShadow = '';
+    }
+
+    function calculateDiscoMode() {
+        if ((Math.abs(oldX - event.clientX) + Math.abs(oldY - event.clientY)) > 1200) {
+            runDiscoMode();
+        }
+        oldX = event.clientX;
+        oldY = event.clientY;
+    }
+
+    function runDiscoMode() {
+        setInterval(() => {
+            const colors = ['red', 'green', 'blue', 'magenta', 'yellow'];
+            document.body.style.backgroundColor = colors[Math.floor(Math.random() * 100 % 5)];
+        }, 200);
     }
 })();
