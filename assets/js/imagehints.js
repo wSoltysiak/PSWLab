919625449917e8ed706fb22a3900f76d065d7d8f
Jakub Nadolny <matryl1@me.com>
2017-11-15 15:37:34 +0100

    Poprawa kolekcji images

--- assets/js/imagehints.js
+++ assets/js/imagehints.js
@@ -1,18 +1,10 @@
-const hints = [
-    "Imię wpisz z wielkiej litery.",
-    "Nazwisko wpisz z wielkiej litery.",
-    "Wybierz miesiąc z listy.",
-    "Email musi posiadać znak @",
-    "Numer telefonu składa się z cyfr"];
-var forms = document.images;
+var images = document.images;
 
 var collections = "";
-
-for(var i = 0; i < forms.length; i++){
+for(var i = 0; i < images.length; i++){
     collections += "Adres zdjęcia " + i +": ";
-    collections += forms[i].src;
+    collections += images[i].src;
     collections += "<br>";
 
 }
-
 document.getElementsByClassName("collection-info")[0].innerHTML = collections;
