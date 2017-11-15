87906d8ff932532c6e0def2abec08b5947022a95
Jakub Nadolny <matryl1@me.com>
2017-11-15 15:16:01 +0100

    Wypisywanie kolekcji formularza

---
+++ assets/js/formhints.js
@@ -0,0 +1,21 @@
+const hints = [
+    "Imię wpisz z wielkiej litery.",
+    "Nazwisko wpisz z wielkiej litery.",
+    "Wybierz miesiąc z listy.",
+    "Email musi posiadać znak @",
+    "Numer telefonu składa się z cyfr"];
+var forms = document.forms[0];
+
+var collections = "";
+
+for(var i = 0; i < forms.length; i++){
+    if(forms[i].id != ""){
+    collections += "Nazwa elmentu : "
+    collections += forms[i].id;
+    collections += ", placeholder : ";
+    collections += forms[i].placeholder;
+    collections += "<br>";
+    }
+}
+
+document.getElementsByClassName("collection-info")[0].innerHTML = collections;
