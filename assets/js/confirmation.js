8796f56d4a516f1eb18ade6aaff39e6a0b1edc32
Jakub Nadolny <matryl1@me.com>
2017-11-15 16:10:56 +0100

    Dodanie potwierdzenia przy kliknięciu guzika

---
+++ assets/js/confirmation.js
@@ -0,0 +1,6 @@
+function confirm_reset(){
+    return confirm("Czy jesteś pewny że chcesz wyczyścić formularz?");
+}
+function confirm_submit(){
+    return confirm("Czy jesteś pewny że chcesz przejść dalej?");
+}
