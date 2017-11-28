05d7475143f69b7f41854bda86d42bf13317801d
Wojciech Sołtysiak <soltysiak.wojciech@gmail.com>
2017-11-28 18:01:57 +0100

    Cleanup pracowników

--- pages/team/team.view.php
+++ pages/team/team.view.php
@@ -1,82 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Galeria biblioteki miejskiej">
-    <meta name="keywords"
-          content="Biblioteka, Miejska, Książki, Wypożyczenia, Biblioteka Miejska, Czytanie, Czytelnictwo, Edukacja, Galeria, Zdjęcia">
-    <meta name="author" content="Jakub Nadolny, Wojciech Sołtysiak">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700&amp;subset=latin-ext" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/welcome-header.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/info-section.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/aside-info.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/table.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/main-footer.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/dropdown-menu.css">
-    <title>Biblioteka miejska - pracownicy</title>
-</head>
-<body>
-<header class="welcome-header">
-    <nav class="dropdown-menu">
-        <ul class="dropdown-menu__first-level">
-            <li id="category1">Kategoria 1
-                <ul id="category1__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                    <li>Podkategoria 1</li>
-                    <li>Podkategoria 2</li>
-                    <li id="subcategory1">Podkategoria 3
-                        <ul id="subcategory1__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
-                            <li>Podkategoria 1</li>
-                            <li>Podkategoria 2</li>
-                            <li>Podkategoria 3</li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li id="category2">Kategoria 2
-                <ul id="category2__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                    <li>Podkategoria 1</li>
-                    <li>Podkategoria 2</li>
-                    <li id="subcategory2">Podkategoria 3
-                        <ul id="subcategory2__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
-                            <li>Podkategoria 1</li>
-                            <li>Podkategoria 2</li>
-                            <li>Podkategoria 3</li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li id="category3">Kategoria 3
-                <ul id="category3__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                    <li>Podkategoria 1</li>
-                    <li id="subcategory3">Podkategoria 2
-                        <ul id="subcategory3__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
-                            <li>Podkategoria 1</li>
-                            <li>Podkategoria 2</li>
-                            <li>Podkategoria 3</li>
-                        </ul>
-                    </li>
-                    <li>Podkategoria 3</li>
-                </ul>
-            </li>
-            <li><a href="pages/home/index.html">Strona główna</a></li>
-            <li><a href="pages/gallery/gallery.html">Galeria</a></li>
-            <li><a href="team.html"><b>Pracownicy</b></a></li>
-            <li><a href="form.html">Zajęcia biblioteczne</a></li>
-            <li><a href="mailto:bibl@zlotow.pl">Kontakt</a></li>
-            <li><a href="http://www.gminazlotow.pl/">Gmina Złotów</a></li>
-        </ul>
-    </nav>
-    <div class="welcome-header__container">
-        <h1 class="welcome-header__header">
-            Biblioteka miejska
-        </h1>
-        <h2 class="welcome-header__header welcome-header__header--small">
-            w Złotowie
-        </h2>
-    </div>
-</header>
+<link rel="stylesheet" type="text/css" href="assets/css/aside-info.css">
+<link rel="stylesheet" type="text/css" href="assets/css/table.css">
+
 <section class="info-section">
     <h2 class="info-section__header">
         Spis pracownikow biblioteki
@@ -268,8 +192,3 @@
         </aside>
     </article>
 </section>
-<footer class="main-footer">
-    <span class="main-footer__text">&copy; Biblioteka miejska w Złotowie - 2017</span>
-</footer>
-</body>
-</html>
