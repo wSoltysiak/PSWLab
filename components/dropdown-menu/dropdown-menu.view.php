31c299fc0146a933160eddda8ecc11023ce8a7e4
Wojciech Sołtysiak <soltysiak.wojciech@gmail.com>
2017-11-28 20:48:46 +0100

    Wydzielenie dropdown-menu

---
+++ components/dropdown-menu/dropdown-menu.view.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" type="text/css" href="components/dropdown-menu/dropdown-menu.style.css">
+
+<nav class="dropdown-menu">
+    <ul class="dropdown-menu__first-level">
+        <li id="category1">Kategoria 1
+            <ul id="category1__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
+                <li>Podkategoria 1</li>
+                <li>Podkategoria 2</li>
+                <li id="subcategory1">Podkategoria 3
+                    <ul id="subcategory1__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
+                        <li>Podkategoria 1</li>
+                        <li>Podkategoria 2</li>
+                        <li>Podkategoria 3</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li id="category2">Kategoria 2
+            <ul id="category2__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
+                <li>Podkategoria 1</li>
+                <li>Podkategoria 2</li>
+                <li id="subcategory2">Podkategoria 3
+                    <ul id="subcategory2__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
+                        <li>Podkategoria 1</li>
+                        <li>Podkategoria 2</li>
+                        <li>Podkategoria 3</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li id="category3">Kategoria 3
+            <ul id="category3__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
+                <li>Podkategoria 1</li>
+                <li id="subcategory3">Podkategoria 2
+                    <ul id="subcategory3__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
+                        <li>Podkategoria 1</li>
+                        <li>Podkategoria 2</li>
+                        <li>Podkategoria 3</li>
+                    </ul>
+                </li>
+                <li>Podkategoria 3</li>
+            </ul>
+        </li>
+        <li><a href="index.php?page=home">Strona główna</a></li>
+        <li><a href="index.php?page=gallery">Galeria</a></li>
+        <li><a href="index.php?page=team">Pracownicy</a></li>
+        <li><a href="index.php?page=form">Zajęcia biblioteczne</a></li>
+        <li><a href="mailto:bibl@zlotow.pl">Kontakt</a></li>
+        <li><a href="http://www.gminazlotow.pl/">Gmina Złotów</a></li>
+    </ul>
+</nav>
--- components/header/header.view.php
+++ components/header/header.view.php
@@ -1,56 +1,7 @@
-<link rel="stylesheet" type="text/css" href="assets/css/welcome-header.css">
-<link rel="stylesheet" type="text/css" href="assets/css/dropdown-menu.css">
+<link rel="stylesheet" type="text/css" href="components/header/header.style.css">
 
 <header class="welcome-header">
-    <nav class="dropdown-menu">
-        <ul class="dropdown-menu__first-level">
-            <li id="category1">Kategoria 1
-                <ul id="category1__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                    <li>Podkategoria 1</li>
-                    <li>Podkategoria 2</li>
-                    <li id="subcategory1">Podkategoria 3
-                        <ul id="subcategory1__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
-                            <li>Podkategoria 1</li>
-                            <li>Podkategoria 2</li>
-                            <li>Podkategoria 3</li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li id="category2">Kategoria 2
-                <ul id="category2__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                    <li>Podkategoria 1</li>
-                    <li>Podkategoria 2</li>
-                    <li id="subcategory2">Podkategoria 3
-                        <ul id="subcategory2__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
-                            <li>Podkategoria 1</li>
-                            <li>Podkategoria 2</li>
-                            <li>Podkategoria 3</li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li id="category3">Kategoria 3
-                <ul id="category3__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                    <li>Podkategoria 1</li>
-                    <li id="subcategory3">Podkategoria 2
-                        <ul id="subcategory3__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
-                            <li>Podkategoria 1</li>
-                            <li>Podkategoria 2</li>
-                            <li>Podkategoria 3</li>
-                        </ul>
-                    </li>
-                    <li>Podkategoria 3</li>
-                </ul>
-            </li>
-            <li><a href="index.php?page=home">Strona główna</a></li>
-            <li><a href="index.php?page=gallery">Galeria</a></li>
-            <li><a href="index.php?page=team">Pracownicy</a></li>
-            <li><a href="index.php?page=form">Zajęcia biblioteczne</a></li>
-            <li><a href="mailto:bibl@zlotow.pl">Kontakt</a></li>
-            <li><a href="http://www.gminazlotow.pl/">Gmina Złotów</a></li>
-        </ul>
-    </nav>
+    <?php include_once('components/dropdown-menu/dropdown-menu.view.php'); ?>
     <div class="welcome-header__container">
         <h1 class="welcome-header__header">
             Biblioteka miejska
