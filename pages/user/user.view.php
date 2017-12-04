2efa77448f05a85e368a44cf7343026b992e3d8f
Jakub Nadolny <matryl1@me.com>
2017-12-04 15:59:39 +0100

    Stworzenie szkieletu panelu użytkownika
    Użytkownik będzie mógł edytować wygląd palenu użytkownika

---
+++ pages/user/user.view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matryl1
+ * Date: 04.12.2017
+ * Time: 15:58
+ */
--- router.php
+++ router.php
@@ -1,7 +1,7 @@
 <?php
 
 class Router {
-    private $allowedPages = ['home', 'gallery', 'team', 'form'];
+    private $allowedPages = ['home', 'gallery', 'team', 'form', 'user'];
     const DEFAULT_PAGE = 'home';
 
     public function getPageName() {
