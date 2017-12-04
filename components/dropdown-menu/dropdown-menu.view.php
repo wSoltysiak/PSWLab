dc23049d10868bcb8fe6527a5956e86a8cdd6ab3
Jakub Nadolny <matryl1@me.com>
2017-12-04 15:53:39 +0100

    Uporządkowanie nawigacji

--- components/dropdown-menu/dropdown-menu.view.php
+++ components/dropdown-menu/dropdown-menu.view.php
@@ -2,10 +2,13 @@
 
 <nav class="dropdown-menu">
     <ul class="dropdown-menu__first-level">
-        <li id="category1">Kategoria 1
+        <li><a href="index.php?page=home">Strona główna</a></li>
+        <li><a href="index.php?page=gallery">Galeria</a></li>
+        <li><a href="index.php?page=team">Pracownicy</a></li>
+        <li id="category1">Kontakt
             <ul id="category1__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                <li>Podkategoria 1</li>
-                <li>Podkategoria 2</li>
+                <li><a href="mailto:bibl@zlotow.pl">Kontakt</a></li>
+                <li><a href="http://www.gminazlotow.pl/">Gmina Złotów</a></li>
                 <li id="subcategory1">Podkategoria 3
                     <ul id="subcategory1__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
                         <li>Podkategoria 1</li>
@@ -15,37 +18,7 @@
                 </li>
             </ul>
         </li>
-        <li id="category2">Kategoria 2
-            <ul id="category2__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                <li>Podkategoria 1</li>
-                <li>Podkategoria 2</li>
-                <li id="subcategory2">Podkategoria 3
-                    <ul id="subcategory2__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
-                        <li>Podkategoria 1</li>
-                        <li>Podkategoria 2</li>
-                        <li>Podkategoria 3</li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-        <li id="category3">Kategoria 3
-            <ul id="category3__submenu" class="dropdown-menu__box dropdown-menu__box--submenu">
-                <li>Podkategoria 1</li>
-                <li id="subcategory3">Podkategoria 2
-                    <ul id="subcategory3__submenu" class="dropdown-menu__box dropdown-menu__box--subsubmenu">
-                        <li>Podkategoria 1</li>
-                        <li>Podkategoria 2</li>
-                        <li>Podkategoria 3</li>
-                    </ul>
-                </li>
-                <li>Podkategoria 3</li>
-            </ul>
-        </li>
-        <li><a href="index.php?page=home">Strona główna</a></li>
-        <li><a href="index.php?page=gallery">Galeria</a></li>
-        <li><a href="index.php?page=team">Pracownicy</a></li>
         <li><a href="index.php?page=form">Zajęcia biblioteczne</a></li>
-        <li><a href="mailto:bibl@zlotow.pl">Kontakt</a></li>
-        <li><a href="http://www.gminazlotow.pl/">Gmina Złotów</a></li>
+        <li><a href="index.php?page=user">Panel Użytkownika</a></li>
     </ul>
 </nav>
