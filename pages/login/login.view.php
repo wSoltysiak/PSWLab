f43c88479e380bd8d1b02dbdb9cdeb7f6d8c30e7
Wojciech Sołtysiak <soltysiak.wojciech@gmail.com>
2017-12-05 15:34:46 +0100

    Dodanie komponentu login-box

---
+++ pages/login/login.view.php
@@ -0,0 +1,7 @@
+<div class="info-section">
+    <form action="index.php?page=login" method="POST">
+        <?php
+            include('components/login-box/login-box.view.php');
+        ?>
+    </form>
+</div>
