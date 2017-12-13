414e7c145c90c36684c95455ce91e657720a6433
Wojciech Sołtysiak <soltysiak.wojciech@gmail.com>
2017-12-13 17:16:38 +0100

    Poprawienie ścieżek do plików css

--- utils/head.php
+++ utils/head.php
@@ -10,10 +10,10 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="/assets/css/jumbotron.css">
-    <link rel="stylesheet" href="/assets/css/carousell.css">
-    <link rel="stylesheet" href="/assets/css/footer.css">
-    <link rel="stylesheet" href="/assets/css/feedback.css">
+    <link rel="stylesheet" href="./assets/css/jumbotron.css">
+    <link rel="stylesheet" href="./assets/css/carousell.css">
+    <link rel="stylesheet" href="./assets/css/footer.css">
+    <link rel="stylesheet" href="./assets/css/feedback.css">
     <title>Biblioteka miejska</title>
 </head>
 <body>
